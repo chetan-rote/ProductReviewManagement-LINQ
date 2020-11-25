396c43ace32e243d31514e46168a680ae4f050b3
Chetan <Chetan@DESKTOP-9SU09BS>
2020-11-25 17:45:07 +0530

    [Chetan]Add. method to get top rated products.

---
+++ ProductReviewManagement/ProductReviewManagement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ProductReviewManagement
+{
+    class ProductReviewManagement
+    {
+        /// <summary>
+        /// Top rated 3 products are selected from list.
+        /// </summary>
+        /// <param name="listProductReview">The list product review.</param>
+        public void TopRecords(List<ProductReview> listProductReview)
+        {
+            var recordedData = (from productReviews in listProductReview
+                                orderby productReviews.Rating descending
+                                select productReviews).Take(3);
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("ProductId: " + list.ProductID + " UserId: " + list.UserID + " Rating: " + list.Rating +
+                    " Review: " + list.Review + " IsLike: " + list.isLike);
+            }
+        }
+    }
+}
