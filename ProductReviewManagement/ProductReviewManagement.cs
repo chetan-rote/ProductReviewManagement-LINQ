d1d61e13588c5c127b82b3a242c6cd6ff71ed3f3
Chetan <Chetan@DESKTOP-9SU09BS>
2020-11-26 13:35:14 +0530

    [Chetan]Add. Method to get ratings greater than 3 for particular product id.

--- ProductReviewManagement/ProductReviewManagement.cs
+++ ProductReviewManagement/ProductReviewManagement.cs
@@ -22,5 +22,21 @@ namespace ProductReviewManagement
                     " Review: " + list.Review + " IsLike: " + list.isLike);
             }
         }
+        /// <summary>
+        /// UC3 Finds ratings greater than three of specific products.
+        /// </summary>
+        /// <param name="listProductReview">The list product review.</param>
+        public void RatingsGreaterThanThreeOfSpecificProducts(List<ProductReview> listProductReview)
+        {
+            var recordedData = from productReview in listProductReview
+                               where (productReview.ProductID == 1 || productReview.ProductID == 2 || productReview.ProductID == 4)
+                               && productReview.Rating > 3
+                               select productReview;
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("ProductId: " + list.ProductID + " UserId: " + list.UserID + " Rating: " + list.Rating +
+                    " Review: " + list.Review + " IsLike: " + list.isLike);
+            }
+        }
     }
 }
